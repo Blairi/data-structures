dc049d8c1378a5ade34f0b41275ef25d4c8c0349
Blairi <axel.avi.fer@gmail.com>
2023-02-06 16:53:38 -0600

    counters added

--- Stack/Stack.cpp
+++ Stack/Stack.cpp
@@ -34,6 +34,26 @@ T pop(Stack<T> *s)
     return value;
 }
 
+template <typename T>
+T pop_steps(Stack<T> *s, int &counter)
+{
+    counter ++;
+
+    if( empty(s) ) // If the stack is empty
+	throw std::runtime_error("The Stack is empty");
+
+    Node<T> *node_tmp = s->top;
+    T value = node_tmp->value;
+
+    // If there is more than one element in the stack
+    if(node_tmp->pNext)
+	s->top = node_tmp->pNext;
+    // If only there an element in the stack
+    else
+	s->top = nullptr;
+
+    return value;
+}
 
 template <typename T>
 bool push(Stack<T> *s, T value)
@@ -50,6 +70,22 @@ bool push(Stack<T> *s, T value)
     return true;
 }
 
+template <typename T>
+bool push_steps(Stack<T> *s, T value, int &steps)
+{
+    steps ++;
+
+    Node<T> *node = createNode<T>(value);
+    if( empty(s) ) // If the stack is empty
+    {
+	s->top = node;
+	return true;
+    }
+
+    node->pNext = s->top;
+    s->top = node;
+    return true;
+}
 
 template <typename T>
 bool empty(Stack<T> *s)
@@ -99,6 +135,29 @@ int search(Stack<T> *s, T key)
     return -1;
 }
 
+template <typename T>
+int search_steps(Stack<T> *s, T key, int &counter)
+{
+    Node<T> *cur = s->top;
+
+    int pos = 0;
+    while(cur)
+    {
+	counter ++;
+
+	T cur_value = cur->value;
+
+	if(cur_value == key) return pos;
+
+	pos ++;
+	cur = cur->pNext;
+    }
+
+    delete cur;
+
+    // If the key isn't in the stack
+    return -1;
+}
 
 template <typename T>
 T at_stack(Stack<T> *s, int pos)
@@ -111,6 +170,33 @@ T at_stack(Stack<T> *s, int pos)
     int i = 0;
     while(i < pos)
     {
+	i ++;
+
+	if(!cur->pNext) break;
+	cur = cur->pNext;
+    }
+
+    if(!cur)
+	std::runtime_error("Illegal position.");
+
+    T value = cur->value;
+
+    return value;
+}
+
+template <typename T>
+T at_stack_steps(Stack<T> *s, int pos, int &counter)
+{
+    if(pos < 0)
+	std::runtime_error("Illegal position.");
+
+    Node<T> *cur = s->top;
+
+    int i = 0;
+    while(i < pos)
+    {
+	counter ++;
+
 	i ++;
 
 	if(!cur->pNext) break;
--- Stack/Stack.h
+++ Stack/Stack.h
@@ -13,9 +13,13 @@ Stack<T> *create_stack();
 
 template <typename T>
 T pop(Stack<T> *s);
+template <typename T>
+T pop_steps(Stack<T> *s, int &counter);
 
 template <typename T>
 bool push(Stack<T> *s, T value);
+template <typename T>
+bool push_steps(Stack<T> *s, T value, int &counter);
 
 template <typename T>
 bool empty(Stack<T> *s);
@@ -25,9 +29,13 @@ void print_stack(Stack<T> *s);
 
 template <typename T>
 int search(Stack<T> *s, T key);
+template <typename T>
+int search_steps(Stack<T> *s, T key, int &counter);
 
 template <typename T>
 T at_stack(Stack<T> *s, int pos);
+template <typename T>
+T at_stack_steps(Stack<T> *s, int pos, int &counter);
 
 #endif
 
